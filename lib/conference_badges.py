1e58b6f51797ff80a3f6b25b98108b3afd727e23
naomiinyele <inyeleinyele@gmail.com>
2023-11-28 17:34:38 +0300

    Assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,8 +1,15 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+names = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+badges = [badge_maker(name) for name in names]
+
+for badge in badges:
+    print(badge)
 
 def batch_badge_creator(names):
-    return None
+    
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
     return None
